e6fe1a4d883153b27c5c86336df11288ba23359d
Joshua Henry <henry41043@gmail.com>
2025-09-29 23:32:20 -0400

    added more menu items and updated home image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {Home} from './home/home';
 import {About} from './about/about';
+import {Resume} from './resume/resume';
+import {Projects} from './projects/projects';
+import {Contact} from './contact/contact';
 
 export const routes: Routes = [
   {
@@ -11,5 +14,17 @@ export const routes: Routes = [
   {
     path: 'about',
     component: About,
+  },
+  {
+    path: 'resume',
+    component: Resume,
+  },
+  {
+    path: 'projects',
+    component: Projects,
+  },
+  {
+    path: 'contact',
+    component: Contact,
   }
 ];
